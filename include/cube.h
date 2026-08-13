5dff01e41c97f3bd7992c326e47061889c8cc76c
GGrzesiek <kryszczuk.grzegorz_4a@op.pl>
2026-08-13 16:47:38 +0200

    added render structs to header

--- include/cube.h
+++ include/cube.h
@@ -1,14 +1,5 @@
 #ifndef CUBE_H
-#define CUBE_H
-
-# define WIN_W 1280
-# define WIN_H 720
-# define TEX_NO 0
-# define TEX_SO 1
-# define TEX_WE 2
-# define TEX_EA 3
-# define TEX_F  4
-# define TEX_C  5
+# define CUBE_H
 
 # include "../libft/libft.h"
 # include <stdio.h>
@@ -16,7 +7,37 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-typedef struct s_img {
+# define WIN_W 1280
+# define WIN_H 720
+
+# define TEX_NO 0
+# define TEX_SO 1
+# define TEX_WE 2
+# define TEX_EA 3
+# define TEX_F 4
+# define TEX_C 5
+
+# define KEY_W 0
+# define KEY_A 1
+# define KEY_S 2
+# define KEY_D 3
+# define KEY_LEFT 4
+# define KEY_RIGHT 5
+# define KEY_COUNT 6
+
+# define K_W 119
+# define K_A 97
+# define K_S 115
+# define K_D 100
+# define K_LEFT 65361
+# define K_RIGHT 65363
+# define K_ESC 65307
+
+# define MOVE_SPEED 3.0
+# define ROT_SPEED 2.0
+
+typedef struct s_img
+{
 	void	*ptr;
 	char	*addr;
 	int		bpp;
@@ -24,38 +45,78 @@ typedef struct s_img {
 	int		endian;
 	int		width;
 	int		height;
-} t_img;
+}	t_img;
 
-typedef struct s_map {
+typedef struct s_map
+{
 	char	*file_name;
-	char	**grid;      /* space-padded rectangle */
+	char	**grid;
 	int		width;
 	int		height;
-} t_map;
+}	t_map;
 
+typedef struct s_scene
+{
+	char	*tex_path[4];
+	int		floor;
+	int		ceiling;
+	t_map	map;
+	double	start_x;
+	double	start_y;
+	char	start_dir;
+}	t_scene;
 
-typedef struct s_scene {          /* <-- Person A fills this, Person B consumes it */
-	char      	*tex_path[4];      /* indexed by TEX_NO..TEX_EA */
-	int			floor;              /* 0xRRGGBB, -1 = unset */
-	int			ceiling;
-	t_map		map;
-	double		start_x;            /* cell centre: (col + 0.5) */
-	double		start_y;
-	char		start_dir;       /* 'N' 'S' 'E' 'W' */
-} t_scene;
+typedef struct s_ray
+{
+	double	dir_x;
+	double	dir_y;
+	double	side_x;
+	double	side_y;
+	double	delta_x;
+	double	delta_y;
+	double	perp_dist;
+	double	wall_x;
+	int		map_x;
+	int		map_y;
+	int		step_x;
+	int		step_y;
+	int		side;
+	int		line_h;
+	int		draw_start;
+	int		draw_end;
+	int		tex_id;
+}	t_ray;
 
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	t_img	frame;
+	t_img	tex[4];
+	t_scene	*scene;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	frame_time;
+	int		keys[KEY_COUNT];
+}	t_game;
 
-int	parse_scene(char *map_file, t_scene *scene);
-int error_msg(char *msg);
+int		parse_scene(char *map_file, t_scene *scene);
+int		run_game(t_scene *scene);
+void	free_scene(t_scene *scene);
+int		error_msg(char *msg);
 
 /*
 	parsing_utils.c
 */
-int is_line_empty(char *line);
-int validate_file(char *map_file);
-int	is_tex_id_used(int tex_ID,  t_scene *scene);
-int handle_unknown_line(char *line, char **first_map_line, int counter);
-int identify_element(char *line);
-int dispatch_element(char *line, int tex_id, t_scene *scene);
+int		is_line_empty(char *line);
+int		validate_file(char *map_file);
+int		is_tex_id_used(int tex_id, t_scene *scene);
+int		handle_unknown_line(char *line, char **first_map_line, int counter);
+int		identify_element(char *line);
+int		dispatch_element(char *line, int tex_id, t_scene *scene);
 
 #endif
